7725802d9556dbb30f004f27a49cf34a056b7ce5
Serge Gebhardt <sgebhar@sgebhar-mbp.local>
2013-11-25 15:48:59 -0200

    Initial Commit

---
+++ OpenCash-Framework/OpenCash_Framework.h
@@ -0,0 +1,13 @@
+//
+//  OpenCash_Framework.h
+//  OpenCash-Framework
+//
+//  Created by Serge Gebhardt on 11/25/13.
+//  Copyright (c) 2013 Serge Gebhardt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenCash_Framework : NSObject
+
+@end
---
+++ OpenCash-Framework/OpenCash_Framework.m
@@ -0,0 +1,13 @@
+//
+//  OpenCash_Framework.m
+//  OpenCash-Framework
+//
+//  Created by Serge Gebhardt on 11/25/13.
+//  Copyright (c) 2013 Serge Gebhardt. All rights reserved.
+//
+
+#import "OpenCash_Framework.h"
+
+@implementation OpenCash_Framework
+
+@end
