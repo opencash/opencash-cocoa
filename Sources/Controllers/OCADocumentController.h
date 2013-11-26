b76ca83d87b87adb482954d08fe69aee8e60e69b
Serge Gebhardt <serge.gebhardt@gmail.com>
2013-11-25 23:40:06 -0200

    added documentcontroller

---
+++ Sources/Controllers/OCADocumentController.h
@@ -0,0 +1,13 @@
+//
+//  OCADocumentController.h
+//  OpenCash-Framework
+//
+//  Created by Serge Gebhardt on 11/25/13.
+//  Copyright (c) 2013 Serge Gebhardt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCADocumentController : NSObject
+
+@end
---
+++ Sources/Controllers/OCADocumentController.m
@@ -0,0 +1,13 @@
+//
+//  OCADocumentController.m
+//  OpenCash-Framework
+//
+//  Created by Serge Gebhardt on 11/25/13.
+//  Copyright (c) 2013 Serge Gebhardt. All rights reserved.
+//
+
+#import "OCADocumentController.h"
+
+@implementation OCADocumentController
+
+@end
